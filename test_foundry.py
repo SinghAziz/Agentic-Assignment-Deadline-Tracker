c31c598dc316e6785096a97a2f1b45402b83dd98
Har Aziz Singh <singhharaziz@gmail.com>
2026-08-31 11:20:48 +0530

    V1 done
    All day fxn is a bit to be fixed, rest works

--- test_foundry.py
+++ test_foundry.py
@@ -1,5 +1,6 @@
 from foundry_local_sdk import Configuration, FoundryLocalManager
-
+from tools import AVAILABLE_FUNCTIONS
+import json
 config = Configuration(app_name = "assignment-deadline-tracker")
 
 FoundryLocalManager.initialize(config)
@@ -26,13 +27,119 @@ else:
 
 model.load()
 
+
+
+add_assignment_tool = {
+        "type" : "function", 
+        "function":{
+                "name" : "add_assignment", 
+                "description" : 
+                "Add an assignment with its due date and optional due time. If due time is not provided, it defaults to 'All Day'.",
+                "parameters" : {
+                        "type" : "object", 
+                        "properties" : {
+                                "assignment":{
+                                        "type" : "string", 
+                                        "description" : "The name of the assignment."
+                                
+                                }, 
+                                "due_date" : {
+                                        "type" : "string", 
+                                        "description" : "The due date of the assignment in YYYY-MM-DD format."
+                                },
+                                "due_time" : {
+                                        "type" : "string", 
+                                        "description" : "The due time of the assignment in HH:MM format. If not provided, it defaults to 'All Day'."
+                                }
+                        }, 
+                        "required" : ["assignment", "due_date"]
+        }
+        }
+}
+
+list_assignment_tool = {
+        "type" : "function", 
+        "function":{
+                "name" : "list_assignments",
+                "description" : "Lists all the assignments that are due on a given date and time. If due time is not provided, it lists all assignments due on the given date regardless of the time.",
+                "parameters" : {
+                        "type" : "object", 
+                        "properties" : {
+                                "due_date" : {
+                                        "type" : "string", 
+                                        "description" : "The due date of the assignment in YYYY-MM-DD format."
+                                },
+                                "due_time" : {
+                                        "type" : "string", 
+                                        "description" : "The due time of the assignment in HH:MM format. If not provided, it defaults to 'All Day'."
+                                }
+                        }, 
+                        "required" : ["due_date"]
+                },
+        }
+}
 client = model.get_chat_client()
 
-response = client.complete_chat([
-        {
-                "role" : "user", 
-                "content" : "Explain what an AI agent is in one sentence."
-        }
-])
+TOOLS =[
+        add_assignment_tool,
+        list_assignment_tool
+]
 
-print(response.choices[0].message.content)
+
+def run_agent(user_input):
+        messages=[
+                {
+                        "role"  : "user",
+                        "content" : user_input
+                }
+        ]
+        while True:
+                print("DEBUG MESSAGES:", messages)
+                response = client.complete_chat(
+                        messages, 
+                        tools = TOOLS
+                )
+                message = response.choices[0].message
+                if not message.tool_calls:
+                        return message.content
+
+                tool_calls =[]
+                for tool_call in message.tool_calls:
+                        tool_calls.append({
+                                "id" : tool_call.id, 
+                                "type" : "function",
+                                "function" : {
+                                        "name" : tool_call.function.name, 
+                                        "arguments" : tool_call.function.arguments
+                                }
+                        })
+                
+                messages.append({
+                "role" : "assistant", 
+                "content" : message.content or "", 
+                "tool_calls" : tool_calls
+        })
+
+                for tool_call in message.tool_calls:
+                        tool_name = tool_call.function.name
+                        arguments = json.loads(
+                                tool_call.function.arguments
+                        )
+
+                        function = AVAILABLE_FUNCTIONS[tool_name]
+                        result = function(**arguments)
+
+                        messages.append({
+                                "role" : "tool",
+                                "tool_call_id" : tool_call.id,
+                                "content" : json.dumps(result)
+                        })
+
+while True:
+        user_input = input("\nYou: ")
+        if user_input.lower() in ["exit", "quit"]:
+                print("Goodbye!")
+                break
+        answer = run_agent(user_input)
+
+        print("\nAgent:", answer)
--- tools.py
+++ tools.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 assignmentList = []
-results = []
 def add_assignment(assignment, due_date, due_time = "All Day"):
     '''
     The function takes assignment, due_date, due_time as inputs.
@@ -15,32 +14,19 @@ def add_assignment(assignment, due_date, due_time = "All Day"):
         })
     return f"Assignment: {assignment} has been added with due date: {due_date}and due time: {due_time}."
 
-def list_assignments(due_date, due_time = None):
+def list_assignments(due_date, due_time = "All Day"):
     '''
     This function takes due_date and due_time as inputs and lists all the assignments that are due on the given date and time.
     If due_time is not provided, it lists all assignments due on the given date regardless of the time.
     '''
+    results = []
     free = True
     for assignment in assignmentList:
-        if (due_time is None and assignmentList[assignment]["Due Date"] == due_date):
-            if (assignmentList[assignment][1] is None):
-                results.append({
-                    "Assignment" : assignment, 
-                    "Due Date" : due_date, 
-                    "Due Time" : "All Day"
-                })
-            else:
-                results.append({
-                    "Assignment" : assignment, 
-                    "Due Date" : due_date, 
-                    "Due Time" : assignmentList[assignment][1]
-                })
-            free = False
-        elif assignmentList[assignment][0] == due_date and assignmentList[assignment][1] == due_time:
+        if assignment["Due Date"] == due_date and ( assignment["Due Time"] == due_time or due_time == "All Day"):
             results.append({
-                "Assignment" : assignment, 
-                "Due Date" : due_date, 
-                "Due Time" : due_time
+                "Assignment" : assignment["Assignment"], 
+                "Due Date" : assignment["Due Date"], 
+                "Due Time" : assignment["Due Time"]
             })
             free = False
 
@@ -50,12 +36,17 @@ def list_assignments(due_date, due_time = None):
     return results
     
 
-
+AVAILABLE_FUNCTIONS={
+    "add_assignment" : add_assignment,
+    "list_assignments" : list_assignments
+}
 # add_assignment("Agentic AI", "2024-06-30")
-# add_assignment("Agentic AI", "2024-06-30", "17:00")
+# add_assignment("Agentic AI", "All Day", "17:00")
 # add_assignment("Mathematics", "2024-06-30")
 # add_assignment("English", "2024-06-30", "1:00")
 # print("Assignments due on 2024-06-30 at 17:00:")
-# list_assignments("2024-06-30", "17:00")
+# print(list_assignments("2024-06-30", "17:00"), sep ="\n")
 # print("Assignments due on 2024-06-30:")
-# list_assignments("2024-06-30")
+# for i in list_assignments("2024-06-30"):
+#     for j in i:
+#         print(f"{j}: {i[j]}")
